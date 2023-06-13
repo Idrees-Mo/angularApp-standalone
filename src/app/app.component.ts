a52ce4629d89f1866177eea9165f53ac82cc5b3d
Muhammad Idrees <Idrees@192-168-1-2.tpgi.com.au>
2023-06-13 22:55:18 +1000

    setup basic register standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './auth/components/register/register.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RegisterComponent, RouterOutlet],
 })
-export class AppComponent {
-  title = 'mediumclone_angular';
-}
+export class AppComponent {}
