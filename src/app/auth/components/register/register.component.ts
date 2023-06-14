2981573d580035a71c6ac30c845783d89f484687
Muhammad Idrees <Idrees@192-168-1-6.tpgi.com.au>
2023-06-14 20:48:53 +1000

    add registerRequestResponse interface

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+  }
+}
