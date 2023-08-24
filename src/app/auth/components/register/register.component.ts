579b29ae8cbb9f7a722384ceacf6f8abcb676378
Muhammad Idrees <idris@y7mail.com>
2023-08-24 19:45:12 +1000

    created isSubmitting selector and used in the register component

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -4,12 +4,15 @@ import { RegisterRequestInterface } from '../../types/registerRequest.interface'
 import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
 import { RouterLink } from '@angular/router';
+import { AuthStateInterface } from '../../types/authState.interface';
+import { selectIsSubmitting } from '../../store/selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
 })
 export class RegisterComponent {
   form = this.fb.nonNullable.group({
@@ -18,7 +21,12 @@ export class RegisterComponent {
     password: ['', Validators.required],
   });
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  $isSubmitting = this.store.select(selectIsSubmitting);
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ auth: AuthStateInterface }>
+  ) {}
 
   onSubmit() {
     const request: RegisterRequestInterface = {
