40dd5cacf38026806accdaeb80c44b81e1fee3be
Muhammad Idrees <idris@y7mail.com>
2023-09-14 21:21:55 +1000

    renamed globalFeed and added feed component

--- src/app/globalFeed/components/globalFeed/globalFeed.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mc-global-feed',
-  templateUrl: './globalFeed.html',
-  standalone: true,
-})
-export class GlobalFeedComponent {}
--- src/app/globalFeed/globalFeed.routes.ts
+++ src/app/globalFeed/globalFeed.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import { GlobalFeedComponent } from './components/globalFeed/globalFeed';
+import { GlobalFeedComponent } from './components/globalFeed/globalFeed.component';
 
 export const routes: Routes = [{ path: '', component: GlobalFeedComponent }];
