4cbc72cef05e9bf830bdb9fd990366565a7f009d
Muhammad Idrees <idris@y7mail.com>
2023-09-21 20:19:44 +1000

    removed unwanted code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { authActions } from './auth/store/actions';
-import { selectCurrentUser } from './auth/store/reducers';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
@@ -14,13 +12,7 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
   imports: [RouterOutlet, RouterLink, CommonModule, NavbarComponent],
 })
 export class AppComponent implements OnInit {
-  currentUser$ = this.store.select(selectCurrentUser);
   constructor(private store: Store) {}
 
-  ngOnInit(): void {
-    this.store.dispatch(authActions.getCurrentUser());
-    this.currentUser$.subscribe((data) => {
-      console.warn(data);
-    });
-  }
+  ngOnInit(): void {}
 }
