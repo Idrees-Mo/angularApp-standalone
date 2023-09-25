cb6254cb5d1b58e0dfc8a2fd913c72ba77a4dd8b
Muhammad Idrees <idris@y7mail.com>
2023-09-25 17:33:10 +1000

    feed markup markup

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -2,16 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { feedActions } from './store/actions';
+import { combineLatest } from 'rxjs';
+import { selectError, selectFeedData, selectIsLoading } from './store/reducer';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl = '';
-  store = inject(Store);
+  private store = inject(Store); // another way of dependency injection
+
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    feed: this.store.select(selectFeedData),
+  });
 
   ngOnInit(): void {
     this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }));
