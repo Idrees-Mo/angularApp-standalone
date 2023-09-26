a11b6c4b2294dd183bf6ed7542fd2c66dba5ab69
Muhammad Idrees <idris@y7mail.com>
2023-09-26 15:03:05 +1000

    adde helpers compnent, banner loading & error

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -5,12 +5,19 @@ import { feedActions } from './store/actions';
 import { combineLatest } from 'rxjs';
 import { selectError, selectFeedData, selectIsLoading } from './store/reducer';
 import { RouterModule } from '@angular/router';
+import { errorMessageComponent } from '../errorMessge/errorMessage.component';
+import { loadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    errorMessageComponent,
+    loadingComponent,
+  ],
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl = '';
